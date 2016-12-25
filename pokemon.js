02c30af619fa78891af23c6d41f89a9aa3c1f4cc
Krishna Kandula <krishnakandula@Krishnas-MBP.fios-router.home>
2016-12-24 19:17:44 -0600

    Program now writes data to json file

--- pokemon.js
+++ pokemon.js
@@ -1,9 +1,41 @@
 class Pokemon {
 	constructor(id, name, type1, type2){
-		this.id = id;
-		this.name = name;
-		this.type1 = type1;
-		this.type2 = type2;
+		this._id = id;
+		this._name = name;
+		this._type1 = type1;
+		this._type2 = type2;
+	}
+	
+	get id () {
+		return this._id;
+	}
+
+	set id (id) {
+		this._id = id;
+	}
+	
+	get name () {
+		return this._name;
+	}
+	
+	set name (name) {
+		this._name = name;
+	}
+	
+	get type1 () {
+		return this._type1;
+	}
+	
+	set type1 (type1) {
+		this._type1 = type1;
+	}
+	
+	get type2 () {
+		return this._type2;
+	}
+	
+	set type2 (type2) {
+		this._type2 = type2;
 	}
 }
 
--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const axios = require('axios');
 const pokemon = require('./pokemon');
-const baseUrl = 'http://pokeapi.co/api/v2/pokemon/1';
+const jf = require('jsonfile');
 
-let p = new pokemon.Pokemon(1, 'hello');
-console.log(p.id, p.name);
+let start = 1;
+let end = 10;
 
-axios.get(baseUrl).then(response => {
-	console.log(response.data);
-}).catch(error => {
-	console.log(error);
-});
+let getDataFromService = (start, pokemonList, end) => {
+	let baseUrl = `http://pokeapi.co/api/v2/pokemon/${start}`;
+	if(start > end){
+		console.log(JSON.stringify(pokemonList));
+		let obj = {name: 'Test'};
+		jf.writeFile('./data.json', pokemonList, (error) => {
+			console.log(error);
+		});
+	}
+	else {
+		console.log(start);
+		axios.get(baseUrl).then(response => {
+			pokemonList.push(getPokemonFromData(response.data));
+			getDataFromService(start + 1, pokemonList, end);
+		}).catch(error => {
+			
+		});
+	}
+}
+
+let getPokemonFromData = (data) => {
+	let p = new pokemon.Pokemon();
+	
+	p.id = data.id;
+	p.name = data.name;
+	p.type1 = data.types[0].type.name;
+	if (data.types.length > 1)
+		p.type2 = data.types[1].type.name;
+	
+	return p;
+}
+
+getDataFromService(start, [], end);
